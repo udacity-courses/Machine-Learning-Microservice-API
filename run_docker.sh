f74ea2f876f6c4702e06ae59a74264954e433002
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-05-04 20:31:31 +0200

    adding bash script to build and run the docker container

--- run_docker.sh
+++ run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build
+docker build -t anyulled/prediction:latest .
 
 # Step 2:
 # List docker images
-docker ps
+docker image ls
 
 # Step 3:
 # Run flask app
-docker run
+docker run -p 80:80 anyulled/prediction:latest
